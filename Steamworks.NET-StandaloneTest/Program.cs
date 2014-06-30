5de32e1dc97a103084987e4ae1bbe109b6414461
Andrea <roundcrisis@gmail.com>
2014-06-30 08:52:45 +0100

    Some test with callbacks

--- Steamworks.NET-StandaloneTest/Program.cs
+++ Steamworks.NET-StandaloneTest/Program.cs
@@ -1,33 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Steamworks;
 
 namespace SteamworksNET_StandaloneTest
 {
     class Program
     {
-        static void Main(string[] args)
+	    private static bool _steamInited;
+		private static 	SteamAPIWarningMessageHook_t SteamAPIWarningMessageHook;
+ 
+	    private static AutoResetEvent _waitForCallback;
+	    private static CGameID _gameId;
+
+	    static void Main(string[] args)
         {
-            bool ret = SteamAPI.Init();
-            if(!ret)
+	        if (!SteamAPI.IsSteamRunning())
+	        {
+		        Console.WriteLine("Please start steam");
+				return;
+	        }
+
+		    _steamInited = SteamAPI.Init();
+		    _gameId = new CGameID(SteamUtils.GetAppID());
+		    Thread.Sleep(100);
+
+            if(!_steamInited)
             {
                 Console.WriteLine("SteamAPI.Init() failed!");
                 return;
             }
+			Console.WriteLine("Packsize.Test() returned: {0}", Packsize.Test());
 
-            ret = Packsize.Test();
-            Console.WriteLine("Packsize.Test() returned: " + ret);
+			SteamAPIWarningMessageHook = new SteamAPIWarningMessageHook_t(SteamAPIDebugTextHook);
+			SteamClient.SetWarningMessageHook(SteamAPIWarningMessageHook);
 
-            while (!Console.KeyAvailable)
-            {
-                SteamAPI.RunCallbacks();
-                Thread.Sleep(100);
-            }
+			Callback<UserStatsReceived_t>.Create(OnUserStatsReceived);
+		    
+		    //    Callback<LeaderboardScoresDownloaded_t>.Create(OnLeaderboardDataReceived);
+			
+
+			Task.Factory.StartNew(() => {
+				                            while (true)
+				                            {
+					                            SteamAPI.RunCallbacks();
+												Thread.Sleep(100);
+				                            }
+				}
+			);
+
+		    _waitForCallback = new AutoResetEvent(false);
+		    _waitForCallback.WaitOne();
+
+//		    while (!Console.KeyAvailable)
+//            {
+//                SteamAPI.RunCallbacks();
+//				Console.Write("+");
+//                Thread.Sleep(100);
+//            }
 
             SteamAPI.Shutdown();
         }
+		private static void SteamAPIDebugTextHook(int nSeverity, StringBuilder pchDebugText)
+		{
+			Console.WriteLine(pchDebugText);
+		}
+	
+
+	    private static void OnUserStatsReceived(UserStatsReceived_t userStatsReceived)
+	    {
+			if (!_steamInited)
+				return;
+		    Console.WriteLine("gameId {0}",userStatsReceived.m_nGameID);
+			Console.WriteLine("userId {0}", userStatsReceived.m_steamIDUser);
+			Console.WriteLine("result {0}", userStatsReceived.m_eResult);
+			Console.WriteLine("user : {0}", SteamUser.GetHSteamUser());
+			Console.WriteLine("steam level : {0}", SteamUser.GetPlayerSteamLevel());
+			Console.WriteLine("steam id : {0}", SteamUser.GetSteamID());
+			int totalGamesPlayed;
+			SteamUserStats.GetStat("NumGames", out totalGamesPlayed);
+			Console.WriteLine("Games played {0}", totalGamesPlayed);
+		    _waitForCallback.Set();
+
+	    }
+
+	    public static void GetLeaderBoard()
+	    {
+			SteamAPICall_t leaderboard = SteamUserStats.FindOrCreateLeaderboard("Score", ELeaderboardSortMethod.k_ELeaderboardSortMethodDescending,
+	ELeaderboardDisplayType.k_ELeaderboardDisplayTypeNumeric);
+
+		     
+	    }
+
+	    private static void OnLeaderboardDataReceived(LeaderboardScoresDownloaded_t leaderboardScoresDownloaded)
+	    {
+		    Console.WriteLine("Count {0}",leaderboardScoresDownloaded.m_cEntryCount);
+		    Console.WriteLine("leaderboard {0}",leaderboardScoresDownloaded.m_hSteamLeaderboard);
+		    Console.WriteLine("leaderboard {0}",leaderboardScoresDownloaded.m_hSteamLeaderboardEntries);
+		    
+	    }
     }
+
+	
 }
